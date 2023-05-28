f46886831b2d3918c464eb8c710d413c3f23e579
ifwlzs <93xo.ox39@gmail.com>
2023-05-28 16:28:39 +0800

    yuedu A

--- action_google_build.sh
+++ action_google_build.sh
@@ -6,8 +6,8 @@ function app_build()
     debug "build with gradle"
     cd $APP_WORKSPACE
     cat gradle.properties
-    chmod a+x gradlew   
-    ./gradlew assembleAppRelease --build-cache --parallel --daemon --warning-mode all
+    chmod +x gradlew   
+    ./gradlew assembleGoogleRelease --build-cache --parallel --daemon --warning-mode all
     
     APP_BUILD_APK=$(find $APP_WORKSPACE/app/build -regex .*/app/release/.*.apk)
     debug "build apk $APP_BUILD_APK"
